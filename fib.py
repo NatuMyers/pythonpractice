deb3da7e6669f1b8d796710ff22d6391653afacc
Ima Okonny <imaokonny@Imas-MacBook-Pro.local>
2016-07-31 23:02:34 -0400

    push

--- fib.py
+++ fib.py
@@ -1,23 +1,14 @@
 
-#it
-init = 1
+# fib naive is naive because its exponential time
+# and recalcs stuff
+
+def fibnaive(n):
+	if (n <= 1):
+		return n
+	else:
+		return fib(n-1)+fib(n-2)
+
+print(fibnaive(5))
 
 
-itr = 20
-
-
-n = 0
-
-if (itr == 0):
-	n = 0
-
-elif(itr == 1):
-	n = 1
-
-else:
-	while (init < itr):
-		init = init + 2
-		
-		itr = itr - 1
-		
-	print init
+# simple map object, m, which maps each value of fib that has already been calculated to its result, and we modify our function to use it and update it.
