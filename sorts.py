6f180942b8f6e3f0cd329920ab271f2196e797ca
Ima Okonny <imaokonny@Imas-MacBook-Pro.local>
2016-08-02 01:37:51 -0400

    push

---
+++ sorts.py
@@ -0,0 +1,8 @@
+def bubble(arr):
+
+	for i in range(len(arr)-1):
+
+		if (arr[i] < arr[i-1]):
+			
+			temp = arr[i]
+			arr[ 
---
+++ sorts2.py
@@ -0,0 +1,22 @@
+
+def msort (arr):
+
+	mid = len(arr)//2
+	l = arr[0:mid]
+	r = arr[mid:]
+	
+	if len(arr) > 1:
+		msort(l)
+		msort(r)
+
+
+	i = 0
+	j = 0
+	k = 0
+
+	while (i < len(l) and j < len(r)):
+
+		if l[i] < r[j]:
+			temp = l[i]
+			l[i] = r[j]
+			l[i] = temp
